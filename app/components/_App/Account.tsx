b59df9b63435b4f622c6f270e4cd1f6d01147881
Lokesh <lokeshwaran100@gmail.com>
2024-05-30 16:38:04 +0000

    Completed initial frontend Integration

--- app/components/_App/Account.tsx
+++ app/components/_App/Account.tsx
@@ -3,193 +3,250 @@ import {
   Flex,
   useDisclosure
 } from '@chakra-ui/react';
-import { ConnectButton } from '@rainbow-me/rainbowkit';
-import { useAccount, useBalance, useNetwork } from 'wagmi';
-import { bscMainnetChainId, bscTestnetChainId, tokenAddresses } from '../../utils/config';
+// import { ConnectButton } from '@rainbow-me/rainbowkit';
+// import { useAccount, useBalance, useNetwork } from 'wagmi';
+// import { bscMainnetChainId, bscTestnetChainId, tokenAddresses } from '../../utils/config';
 import BuyModal from '../Modal/BuyModal'
-import { useDappContext } from '../../utils/Context';
+// import { useDappContext } from '../../utils/Context';
+// import { usePhantomWallet } from "./usePhantomWallet";
+import dynamic from 'next/dynamic';
+import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+
+// const WalletMultiButtonDynamic = dynamic(
+//   async () => (await import('@solana/wallet-adapter-react-ui')).WalletMultiButton,
+//   { ssr: false }
+// );
+
 
 const Account = () => {
+  // const { connectWallet, disconnectWallet, publicKey } = usePhantomWallet();
+  // const onClick = async () => {
+  //   if (!publicKey) {
+  //     await connectWallet();
+  //   } else {
+  //     await disconnectWallet();
+  //   }
+  // };
+  
+  // return <button onClick={onClick}>Connect Wallet</button>;
+  
   const {
     isOpen: isOpen,
     onOpen: onOpen,
     onClose: onClose,
   } = useDisclosure();
-  const { chain } = useNetwork();
-  const { address, } = useAccount();
-  const chainId = chain?.id != undefined ? chain.id :
-    process.env.NEXT_PUBLIC_MAINNET_OR_TESTNET == "mainnet" ? bscMainnetChainId : bscTestnetChainId;
-  console.log(chain)
-  const { data: balanceOfToken, isError, isLoading } = useBalance({
-    address: address,
-    // @ts-ignore
-    token: tokenAddresses[chainId],
-  })
-
-  const {
-    status, setStatus
-  } = useDappContext();
-
   return (
-    <ConnectButton.Custom>
-      {({
-        account,
-        chain,
-        openAccountModal,
-        openChainModal,
-        openConnectModal,
-        authenticationStatus,
-        mounted,
-      }) => {
-        // Note: If your app doesn't use authentication, you
-        // can remove all 'authenticationStatus' checks
-        const ready = mounted && authenticationStatus !== 'loading';
-        const connected =
-          ready &&
-          account &&
-          chain &&
-          (!authenticationStatus ||
-            authenticationStatus === 'authenticated');
+    <>
+    <div>
+      <Button
+        onClick={onOpen}
+        fontWeight="500px"
+        width={['50px', '80px', '80px', '80px']}
+        borderRadius={'35px'}
+        fontSize={['12px', '14px', '16px', '18px']}
+        height={['35px', '35px', '35px', '41px']}
+        color={'white'}
+        border="1px solid #ED2775"
+        backgroundColor={'transparent'}
+        className={'sticky-font'}
+        _hover={{
+          backgroundColor: '#ED2775',
+        }}
+        _active={{
+          backgroundColor: 'transparent',
+        }}
+      >
+        Buy
+      </Button>     
+      <WalletMultiButton />
+      <BuyModal
+        isOpenBuyModal={isOpen}
+        onCloseBuyModal={onClose}
+      />
+    </div>           
+  </>
+  )
 
-        return (
-          <div
-            {...(!ready && {
-              'aria-hidden': true,
-              'style': {
-                opacity: 0,
-                userSelect: 'none',
-                pointerEvents: 'none',
-              },
-            })}
-          >
-            {(() => {
-              if (!connected) {
-                return (
-                  <Button
-                    onClick={openConnectModal}
-                    variant={'solid'}
-                    width={['150px', '150px', '150x', '180px']}
-                    height={['35px', '35px', '35px', '41px']}
-                    borderRadius={['10px']}
-                    backgroundColor={'transparent'}
-                    color={'white'}
-                    border="1px solid #ED2775"
-                    fontSize={['14px', '14px', '16px', '18px']}
-                    fontWeight="500px"
-                    lineHeight={'25px'}
-                    className={'sticky-font'}
-                    _hover={{
-                      backgroundColor: '#ED2775',
-                    }}
-                    _active={{
-                      backgroundColor: 'transparent',
-                    }}
-                  >
-                    Connect Wallet
+  // const {
+  //   isOpen: isOpen,
+  //   onOpen: onOpen,
+  //   onClose: onClose,
+  // } = useDisclosure();
+  // const { chain } = useNetwork();
+  // const { address, } = useAccount();
+  // const chainId = chain?.id != undefined ? chain.id :
+  //   process.env.NEXT_PUBLIC_MAINNET_OR_TESTNET == "mainnet" ? bscMainnetChainId : bscTestnetChainId;
+  // console.log(chain)
+  // const { data: balanceOfToken, isError, isLoading } = useBalance({
+  //   address: address,
+  //   // @ts-ignore
+  //   token: tokenAddresses[chainId],
+  // })
 
-                  </Button>
-                );
-              }
-              if (chain.unsupported) {
-                return (
-                  <Button
-                    onClick={openChainModal}
-                    variant={'solid'}
-                    width={['150px', '150px', '150x', '180px']}
-                    height={['35px', '35px', '35px', '41px']}
-                    borderRadius={['35px', '35px', '35px', '35px']}
-                    border="1px solid #FC541C"
-                    backgroundColor={'red'}
-                    color={'white'}
-                    className={'sticky-font'}
-                    fontSize={['12px', '14px', '16px', '18px']}
-                    fontWeight="500px"
-                    _hover={{
-                      backgroundColor: '#E18833',
-                    }}
-                    _active={{
-                      backgroundColor: 'transparent',
-                    }}
-                  >
-                    Wrong network
-                  </Button>
-                );
-              }
-              return (
-                <Flex
-                  gap={['5px', '10px', '20px', '20px', '20px', '20px']}
-                  alignItems={'center'}
-                >
-                  <Flex
-                    color={'white'}
-                    fontSize={['12px', '14px', '16px', '18px']}
-                    fontWeight="500px"
-                    lineHeight={'25px'}
-                    textAlign={'center'}
-                    className={'sticky-font'}
-                  >
+  // const {
+  //   status, setStatus
+  // } = useDappContext();
 
-                    {status}
-                  </Flex>
-                  <Button
-                    onClick={onOpen}
-                    fontWeight="500px"
-                    width={['50px', '80px', '80px', '80px']}
-                    borderRadius={'35px'}
-                    fontSize={['12px', '14px', '16px', '18px']}
-                    height={['35px', '35px', '35px', '41px']}
-                    color={'white'}
-                    border="1px solid #ED2775"
-                    backgroundColor={'transparent'}
-                    className={'sticky-font'}
-                    _hover={{
-                      backgroundColor: '#ED2775',
-                    }}
-                    _active={{
-                      backgroundColor: 'transparent',
-                    }}
-                  >
-                    Buy
-                  </Button>
-                  <Button
-                    onClick={openAccountModal}
-                    variant={'solid'}
-                    width={['160px', '200px', '200x', '250px']}
-                    height={['35px', '35px', '35px', '41px']}
-                    borderRadius={['35px', '35px', '35px', '35px']}
-                    color={'white'}
-                    border="1px solid #ED2775"
-                    fontSize={['12px', '14px', '16px', '18px']}
-                    fontWeight="500px"
-                    lineHeight={'25px'}
-                    className={'sticky-font'}
-                    backgroundColor={'transparent'}
-                    _hover={{
-                      backgroundColor: '#ED2775',
-                    }}
-                    _active={{
-                      backgroundColor: 'transparent',
-                    }}
-                  >
+  // return (
+  //   <ConnectButton.Custom>
+  //     {({
+  //       account,
+  //       chain,
+  //       openAccountModal,
+  //       openChainModal,
+  //       openConnectModal,
+  //       authenticationStatus,
+  //       mounted,
+  //     }) => {
+  //       // Note: If your app doesn't use authentication, you
+  //       // can remove all 'authenticationStatus' checks
+  //       const ready = mounted && authenticationStatus !== 'loading';
+  //       const connected =
+  //         ready &&
+  //         account &&
+  //         chain &&
+  //         (!authenticationStatus ||
+  //           authenticationStatus === 'authenticated');
 
-                    {account.displayName}
-                    {balanceOfToken?.formatted
-                      ? ` ${Number(balanceOfToken?.formatted).toPrecision(2)}(${balanceOfToken?.symbol})`
-                      : ''}
-                  </Button>
-                  <BuyModal
-                    isOpenBuyModal={isOpen}
-                    onCloseBuyModal={onClose}
-                  />
-                </Flex>
-              );
-            })()}
-          </div>
-        );
-      }}
+  //       return (
+  //         <div
+  //           {...(!ready && {
+  //             'aria-hidden': true,
+  //             'style': {
+  //               opacity: 0,
+  //               userSelect: 'none',
+  //               pointerEvents: 'none',
+  //             },
+  //           })}
+  //         >
+  //           {(() => {
+  //             if (!connected) {
+  //               return (
+  //                 <Button
+  //                   onClick={openConnectModal}
+  //                   variant={'solid'}
+  //                   width={['150px', '150px', '150x', '180px']}
+  //                   height={['35px', '35px', '35px', '41px']}
+  //                   borderRadius={['10px']}
+  //                   backgroundColor={'transparent'}
+  //                   color={'white'}
+  //                   border="1px solid #ED2775"
+  //                   fontSize={['14px', '14px', '16px', '18px']}
+  //                   fontWeight="500px"
+  //                   lineHeight={'25px'}
+  //                   className={'sticky-font'}
+  //                   _hover={{
+  //                     backgroundColor: '#ED2775',
+  //                   }}
+  //                   _active={{
+  //                     backgroundColor: 'transparent',
+  //                   }}
+  //                 >
+  //                   Connect Wallet
 
-    </ConnectButton.Custom>
-  );
+  //                 </Button>
+  //               );
+  //             }
+  //             if (chain.unsupported) {
+  //               return (
+  //                 <Button
+  //                   onClick={openChainModal}
+  //                   variant={'solid'}
+  //                   width={['150px', '150px', '150x', '180px']}
+  //                   height={['35px', '35px', '35px', '41px']}
+  //                   borderRadius={['35px', '35px', '35px', '35px']}
+  //                   border="1px solid #FC541C"
+  //                   backgroundColor={'red'}
+  //                   color={'white'}
+  //                   className={'sticky-font'}
+  //                   fontSize={['12px', '14px', '16px', '18px']}
+  //                   fontWeight="500px"
+  //                   _hover={{
+  //                     backgroundColor: '#E18833',
+  //                   }}
+  //                   _active={{
+  //                     backgroundColor: 'transparent',
+  //                   }}
+  //                 >
+  //                   Wrong network
+  //                 </Button>
+  //               );
+  //             }
+  //             return (
+  //               <Flex
+  //                 gap={['5px', '10px', '20px', '20px', '20px', '20px']}
+  //                 alignItems={'center'}
+  //               >
+  //                 <Flex
+  //                   color={'white'}
+  //                   fontSize={['12px', '14px', '16px', '18px']}
+  //                   fontWeight="500px"
+  //                   lineHeight={'25px'}
+  //                   textAlign={'center'}
+  //                   className={'sticky-font'}
+  //                 >
+
+  //                   {status}
+  //                 </Flex>
+  //                 <Button
+  //                   onClick={onOpen}
+  //                   fontWeight="500px"
+  //                   width={['50px', '80px', '80px', '80px']}
+  //                   borderRadius={'35px'}
+  //                   fontSize={['12px', '14px', '16px', '18px']}
+  //                   height={['35px', '35px', '35px', '41px']}
+  //                   color={'white'}
+  //                   border="1px solid #ED2775"
+  //                   backgroundColor={'transparent'}
+  //                   className={'sticky-font'}
+  //                   _hover={{
+  //                     backgroundColor: '#ED2775',
+  //                   }}
+  //                   _active={{
+  //                     backgroundColor: 'transparent',
+  //                   }}
+  //                 >
+  //                   Buy
+  //                 </Button>
+  //                 <Button
+  //                   onClick={openAccountModal}
+  //                   variant={'solid'}
+  //                   width={['160px', '200px', '200x', '250px']}
+  //                   height={['35px', '35px', '35px', '41px']}
+  //                   borderRadius={['35px', '35px', '35px', '35px']}
+  //                   color={'white'}
+  //                   border="1px solid #ED2775"
+  //                   fontSize={['12px', '14px', '16px', '18px']}
+  //                   fontWeight="500px"
+  //                   lineHeight={'25px'}
+  //                   className={'sticky-font'}
+  //                   backgroundColor={'transparent'}
+  //                   _hover={{
+  //                     backgroundColor: '#ED2775',
+  //                   }}
+  //                   _active={{
+  //                     backgroundColor: 'transparent',
+  //                   }}
+  //                 >
+
+  //                   {account.displayName}
+  //                   {balanceOfToken?.formatted
+  //                     ? ` ${Number(balanceOfToken?.formatted).toPrecision(2)}(${balanceOfToken?.symbol})`
+  //                     : ''}
+  //                 </Button>
+  //                 <BuyModal
+  //                   isOpenBuyModal={isOpen}
+  //                   onCloseBuyModal={onClose}
+  //                 />
+  //               </Flex>
+  //             );
+  //           })()}
+  //         </div>
+  //       );
+  //     }}
+
+  //   </ConnectButton.Custom>
+  // );
 };
 
 export default Account;
